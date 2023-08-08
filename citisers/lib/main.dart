780c792d8b95f6d4b66b76e27a67a7cb8efcc2f2
Himanshu Singh <himanshurpsingh@gmail.com>
2023-08-08 22:42:56 +0530

    more ui

--- citisers/lib/main.dart
+++ citisers/lib/main.dart
@@ -1,8 +1,11 @@
-import 'package:citisers/home_page.dart';
-import 'package:citisers/number_page.dart';
-import 'package:citisers/otp_page.dart';
+import 'package:citisers/pages/home_page.dart';
+import 'package:citisers/pages/number_page.dart';
+import 'package:citisers/pages/otp_page.dart';
+import 'package:citisers/pages/setting.dart';
+import 'package:citisers/utils/theme_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,14 +23,23 @@ class main_page extends StatefulWidget {
 class _main_pageState extends State<main_page> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: 'number_page',
-      routes: {
-        'number_page': (context) => number_page(),
-        'otp_page': (context) => otp_page(),
-        'home_page': (context) => home_page()
-      },
+    return ChangeNotifierProvider(
+      create: (_) => ThemeModel(),
+      child: Consumer<ThemeModel>(
+        builder: (context, themeModel, _) {
+          return MaterialApp(
+            theme: themeModel.isDarkMode ? ThemeData.dark() : ThemeData.light(),
+            debugShowCheckedModeBanner: false,
+            initialRoute: 'number_page',
+            routes: {
+              'number_page': (context) => number_page(),
+              'otp_page': (context) => otp_page(),
+              'home_page': (context) => home_page(),
+              'setting_page': (context) => setting_page(),
+            },
+          );
+        },
+      ),
     );
   }
 }
